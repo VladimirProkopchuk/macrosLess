fde809db2fec5f5fe37600d168edcd9dec93a0f6
Vladimir Prokopchuk <vovan.devil999@gmail.com>
2024-03-05 12:14:51 +0300

    Initial commit

---
+++ modeadd/Add.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+float add(float a, float b) {
+	return a + b;
+}
---
+++ modeadd/modeadd.cpp
@@ -0,0 +1,29 @@
+﻿#include "Add.h"
+#define MODE 1
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+#ifdef MODE
+    cout << "Работаю в боевом режиме." << endl;
+#endif
+#ifndef MODE 
+#error "Опредилите MODE"
+#endif
+
+#if MODE == 1
+    float number1 = 0, number2 = 0;
+    cout << "Введите первое число: ";
+    cin >> number1;
+    cout << "Введите второе число: ";
+    cin >> number2;
+    cout << "Результат сложения: " << add(number1, number2);
+#elif MODE == 0
+    cout << "Работаю в режиме тренировки. " << endl;
+#else
+    cout << "Неизвестный режим.Завершение работы." << endl;
+#endif
+
+}
